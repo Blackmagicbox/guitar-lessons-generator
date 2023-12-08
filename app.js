e482861bdacfdb9c15ada6df9fd06c7fd9141a7c
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-08 12:47:53 +0100

    FEAT: Base structure and GenerateMidiFile method.

---
+++ app.js
@@ -0,0 +1,5 @@
+import {GenerateMidiFile} from "./src/generate-midi-file.js";
+console.log("Am I working?");
+GenerateMidiFile();
+// Path: app.js
+
---
+++ src/generate-midi-file.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param name String
+ * @param notes String[string,number]
+ */
+export const GenerateMidiFile = (name, notes) => {
+  console.log("Have I been called?");
+
+  
+}
