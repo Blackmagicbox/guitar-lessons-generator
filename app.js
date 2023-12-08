42ab37d33d2ff56f7e7e14923705daf76b887c1e
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-08 23:24:51 +0100

    FEAT: Create and name the files according to the parameters received.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import {GenerateMidiFile} from "./src/generate-midi-file.js";
 console.log("Am I working?");
-GenerateMidiFile();
+GenerateMidiFile('MajorScale', [["C4","4"],["D4","4"],["E4","4"],["F4","4"],["G4","4"],["A4","4"],["B4","4"],["C5","4"],["C4","1"]]);
 // Path: app.js
 
--- src/generate-midi-file.js
+++ src/generate-midi-file.js
@@ -1,10 +1,37 @@
+import MidiWriter from "midi-writer-js";
+import * as fs from "fs";
+
 /**
  *
  * @param name String
- * @param notes String[string,number]
+ * @param notes String[]
  */
 export const GenerateMidiFile = (name, notes) => {
   console.log("Have I been called?");
 
-  
+  const track = new MidiWriter.Track();
+
+  track.setTempo(120);
+
+  for (const note of notes) {
+    track.addEvent(new MidiWriter.NoteEvent({
+      pitch: note[0],
+      duration: note[1]
+    }));
+  }
+
+  const write = new MidiWriter.Writer(track);
+  console.log(`${write.dataUri()}`);
+
+  fs.writeFile(getFileName(name), write.buildFile(), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+
 }
+
+
+const getFileName = (name) => {
+  return `${name}.mid`;
+}
+
