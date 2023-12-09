675123a2350f44df82f69be5d86c87320fefb885
Rafael Santos <rafa@blackmagicbox.com.br>
2023-12-09 01:18:33 +0100

    FEAT Express Server with get endpoint

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+import express from "express";
 import {GenerateMidiFile} from "./src/generate-midi-file.js";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 console.log("Am I working?");
-GenerateMidiFile('MajorScale', [["C4","4"],["D4","4"],["E4","4"],["F4","4"],["G4","4"],["A4","4"],["B4","4"],["C5","4"],["C4","1"]]);
+GenerateMidiFile('MajorScale', [["C4", "4"], ["D4", "4"], ["E4", "4"], ["F4", "4"], ["G4", "4"], ["A4", "4"], ["B4", "4"], ["C5", "4"], ["C4", "1"]]);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 // Path: app.js
 
